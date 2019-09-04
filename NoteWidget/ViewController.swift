00df99f3f90c77f3e1787367458fae9a5b80209f
Rafael Rios <Wukengch@gmail.com>
2019-09-04 11:01:49 -0500

    Initial Commit

---
+++ NoteWidget/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NoteWidget
+//
+//  Created by rafael rios garcia on 9/4/19.
+//  Copyright © 2019 ARES. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
